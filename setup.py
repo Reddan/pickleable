20df4a3ec708a8d3e3157b76815b856187da8215
Hampus Hallman <me@hampushallman.com>
2018-04-24 12:10:37 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+  name='pickleable',
+  packages=['pickleable'],
+  version='0.1',
+  author='Hampus Hallman',
+  author_email='me@hampushallman.com',
+  url='https://github.com/Reddan/pickleable',
+  license='MIT',
+  install_requires=[
+    'checkpointer',
+  ],
+  python_requires='~=3.5',
+)
